860b867e35d8a5631e2350a4e1dfa766a2ff3592
Regina Rayssa <cordovileregina@gmail.com>
2024-06-07 15:16:52 -0300

    kk

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ const App = () => {
     const formData = new FormData();
     formData.append('file', file);
     try {
-      const res = await axios.post('https://rhpservidor.onrender.com/upload', formData, {
+      const res = await axios.post('rhphost.ddns.net/upload', formData, {
         headers: {
           'Content-Type': 'multipart/form-data'
         }
@@ -52,7 +52,7 @@ const App = () => {
   const submitValores = async (event) => {
     event.preventDefault();
     try {
-      const res = await axios.post('https://rhpservidor.onrender.com/copiaTabela', {
+      const res = await axios.post('rhphost.ddns.net/copiaTabela', {
         valor1: valor1,
         valor2: valor2
       });
